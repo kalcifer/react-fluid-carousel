718d3d3d0640a3f540abca0bc53f010828dfef0a
pksjce <pksjce@gmail.com>
2018-08-01 20:23:28 +1000

    Publish storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -15,7 +15,14 @@ storiesOf("Carousel", module).add("with text", () => {
         return (
           <div
             key={key}
-            style={{ width: "60px", height: "80px", border: "1px solid" }}
+            style={{
+              width: "60px",
+              height: "80px",
+              border: "1px solid",
+              background: `#${Math.floor(Math.random() * 16777215).toString(
+                16
+              )}` // thanks paul irish!https://www.paulirish.com/2009/random-hex-color-code-snippets/
+            }}
           />
         );
       })}
