8f3928df2313063f25c1ead1655f277e658abd9c
pksjce <pksjce@gmail.com>
2018-08-25 16:05:41 +1000

    simploify

--- src/util.js
+++ src/util.js
@@ -4,35 +4,6 @@ import {
   childHoverVals
 } from "./const";
 
-export const findPrevNextObjs = (findKey, childRefs) => {
-  let prevKeyList = [];
-  let nextKeyList = [];
-  let prevObj;
-  let currentObj;
-  let nextObj;
-  childRefs.forEach(child => {
-    if (child.current) {
-      if (currentObj && !nextObj) {
-        nextObj = child;
-        nextKeyList.push(child.current.getAttribute("data-key"));
-      } else if (currentObj && nextObj && nextKeyList.length > 0) {
-        nextKeyList.push(child.current.getAttribute("data-key"));
-      }
-      if (child.current.getAttribute("data-key") == findKey) {
-        currentObj = child;
-      }
-      if (prevObj && !currentObj && !nextObj) {
-        prevKeyList.push(child.current.getAttribute("data-key"));
-      }
-      if (!prevObj && !currentObj) {
-        prevObj = child;
-        prevKeyList.push(prevObj.current.getAttribute("data-key"));
-      }
-    }
-  });
-  return [prevKeyList, nextKeyList];
-};
-
 export const getChildStyles = (hoveredItem, key) => {
   let childStyles = {};
   if (hoveredItem) {
